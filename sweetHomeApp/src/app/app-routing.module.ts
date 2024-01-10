b33bfac39828c03f06af7f77c207e71e67f4fd89
joaopedrojardim <joaopedrocjardim@gmail.com>
2024-01-10 20:56:15 -0300

    Estrutura inicial da plataforma

--- sweetHomeApp/src/app/app-routing.module.ts
+++ sweetHomeApp/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import("./views/home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: 'details/:id',
+    loadChildren: () => import("./views/details/details.module").then(m => m.DetailsModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import("./views/search/search.module").then(m => m.SearchModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
