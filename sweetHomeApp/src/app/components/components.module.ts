8ff9f5f64d7f31bd54b24283736830c4b9485100
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-02-03 14:57:58 -0300

    Criação do componente SearchBar

--- sweetHomeApp/src/app/components/components.module.ts
+++ sweetHomeApp/src/app/components/components.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 import { SliderComponent } from './slider/slider.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     CardComponent,
-    SliderComponent
+    SliderComponent,
+    SearchBarComponent
   ],
   imports: [
     CommonModule
