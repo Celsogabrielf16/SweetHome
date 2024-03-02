e7de85c6a0e7d0b3487921f684e45596d73c2605
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-03-02 17:30:19 -0300

    Estilização do componente loading

--- sweetHomeApp/frontend/src/app/app.component.ts
+++ sweetHomeApp/frontend/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss',
+    './styles/colors.scss'
+  ]
 })
 export class AppComponent {
   title = 'sweetHomeApp';
