7f46267b43ace49b74997cb13bfcf52ab7204043
Celso Gabriel Prado <celsogabrielf@gmail.com>
2025-03-26 11:29:44 -0300

    Remoção temporária da api do Google Maps no componente Details

--- src/app/services/geocoding.service.ts
+++ src/app/services/geocoding.service.ts
@@ -6,13 +6,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GeocodingService {
-  private apiKey: string = 'AIzaSyB5yMmUVIOFWkq8ZD11E7aWHKI8S91PqIc';
+  private apiKey: string = 'AIzaSyB7HEhAkkTGe4WOuk0QiDW5rJWA1pdNWTQ';
   private apiUrl: string = 'https://maps.googleapis.com/maps/api/geocode/json';
 
   constructor(private http: HttpClient) { }
 
   getCoordinates(location: string): Observable<any> {
     const url = `${this.apiUrl}?address=${encodeURIComponent(location)}&key=${this.apiKey}`;
+
     return this.http.get(url);
+
   }
 }
